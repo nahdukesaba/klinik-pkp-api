3ef464aade22f136762e3c04c18c1be40fec4693
Pusdatin Dev <pusdatin.dev@pkp.go.id>
2026-05-05 08:43:54 +0700

    add admin only access in faq

--- internal/api/v1/faq/routes.go
+++ internal/api/v1/faq/routes.go
@@ -1,11 +1,15 @@
 package faq
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"klinik-pkp-api/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func SetupRoutes(app fiber.Router, handler *Handler) {
 	app.Get("/faqs", handler.GetFAQsHandler)
 	app.Get("/faqs/:id", handler.GetFAQByIdHandler)
-	app.Post("/faqs", handler.PostFAQHandler)
-	app.Put("/faqs/:id", handler.PutFAQHandler)
-	app.Delete("/faqs/:id", handler.DeleteFAQHandler)
+	app.Post("/faqs", utils.AuthMiddleware(), utils.RoleMiddleware("admin"), handler.PostFAQHandler)
+	app.Put("/faqs/:id", utils.AuthMiddleware(), utils.RoleMiddleware("admin"), handler.PutFAQHandler)
+	app.Delete("/faqs/:id", utils.AuthMiddleware(), utils.RoleMiddleware("admin"), handler.DeleteFAQHandler)
 }
